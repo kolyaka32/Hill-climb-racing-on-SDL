d8a0be52a65e20e21e7efa07c23097bb5fa3c928
kolyaka32 <nik.kazankov.05@mail.ru>
2023-09-01 23:56:13 +0300

    Add sky and move of init functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "define.hpp"
 #include "structs.hpp"
 
+#include "init.hpp"
 #include "im_load.hpp"
 #include "perlin.hpp"
 
@@ -23,52 +24,11 @@ bool isCollide(SDL_Rect rect1, SDL_Rect rect2){
     );
 }
 
-// Game classes
-// Base entity in-game class
-class Entity{
-protected:
-    SDL_Texture *texture;
-    SDL_Rect dest;
-public:
-    Entity(){};
-    ~Entity(){};
-    void blit(){
-	    SDL_RenderCopy(app.renderer, texture, NULL, &dest); 
-    };
-};
-
-
-// Function of initialasing 
-void initSDL()
-{
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO);  // Initialising SDL libarary
-
-	app.window = SDL_CreateWindow(WINDOWNAME, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-	app.renderer = SDL_CreateRenderer(app.window, -1, SDL_RENDERER_ACCELERATED);
-	
-    IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);  // Initialasing image library
-    TTF_Init();  // Initialasing fonts library
-    //Mix_Init(MIX_INIT_OGG | MIX_INIT_FLAC);
-    //Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 );
-}
-// Function of closing and deinitialasing all libraries and files
-void cleanup(void)
-{
-    // Closing all outside libraries
-	SDL_DestroyRenderer(app.renderer);
-	SDL_DestroyWindow(app.window);
-	
-    //Mix_CloseAudio();
-    Mix_Quit();
-	SDL_Quit();
-    IMG_Quit();
-    TTF_Quit();
-}
-
 // Main function
 int main(int argv, char** args){
     initSDL();  // Initialasing of main SDL library
     loadAllTextures();  // Loading sprites to the game
+
     SDL_Event event;
     int x = 0; int dx = 0;
 
@@ -82,8 +42,6 @@ int main(int argv, char** args){
     // Main game cycle
 	while(running)
 	{
-        // Showing loading screen
-
         // Getting events
         while( SDL_PollEvent(&event) != 0 ){  
             if (event.type == SDL_QUIT){
@@ -112,14 +70,18 @@ int main(int argv, char** args){
         SDL_SetRenderDrawColor(app.renderer, 77, 143, 172, 255);  // Drawing shield bar
         SDL_RenderClear(app.renderer);
 
+        // Draw sky
+        SDL_RenderCopyEx(app.renderer, Textures[IMG_SKY], NULL, NULL, 0, NULL, SDL_FLIP_NONE);  // Draw ground
+
+        // Drawing terrain
         for(int ddx=0; ddx<SCREEN_WIDTH;++ddx){
             int h = perlin.normalizedOctave1D_01((x+ddx)*0.005, 4, 0.5)*300+250;
             SDL_Rect srcStn = {(x+ddx) % groundW, 0, 1, SCREEN_HEIGHT-h};
             SDL_Rect desStn = {ddx, h, 1, SCREEN_HEIGHT-h};
-            SDL_RenderCopyEx(app.renderer, Textures[IMG_GRND], &srcStn, &desStn, 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(app.renderer, Textures[IMG_GRND], &srcStn, &desStn, 0, NULL, SDL_FLIP_NONE);  // Draw ground
             SDL_Rect srcGrs = {(x+ddx) % groundW, 0, 1, grassH};
             SDL_Rect desGrs = {ddx, h-grassH/2, 1, grassH};
-            SDL_RenderCopyEx(app.renderer, Textures[IMG_SURF], &srcGrs, &desGrs, 0, NULL, SDL_FLIP_NONE);
+            SDL_RenderCopyEx(app.renderer, Textures[IMG_SURF], &srcGrs, &desGrs, 0, NULL, SDL_FLIP_NONE);  // Draw up surface
         }
 
         SDL_RenderPresent(app.renderer);  // Blitting textures on screen
--- src/main.hpp
+++ src/main.hpp
@@ -5,18 +5,6 @@
 // Function of getting collision of 2 objects
 bool isCollide(SDL_Rect rect1, SDL_Rect rect2);
 
-// Game classes
-// Base entity in-game class
-class Entity{
-protected:
-    SDL_Texture *texture;
-    SDL_Rect dest;
-public:
-    Entity();
-    ~Entity();
-    void blit();
-};
-
 // Function of initialasing 
 void initSDL();
 
